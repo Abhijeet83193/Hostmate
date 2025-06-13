43d37a71c9c92b03015c5e2638429fba9a063cc1
Abhijeet Dhokne <abhijeetdhokne1474@gmail.com>
2025-06-13 12:27:55 +0530

    mongoose connect with database 'Hostmate'

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
+const MONGO_URL = mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+async function main(){
+    await mongoose.connect(MONGO_URL);
+}
+
+main()
+    .then( () => {
+        console.log("connected to DB");
+    })
+    .catch( (err) => {
+        console.log(err);
+    });
+
 app.get("/", (req,res) => {
     res.send("I am root");
 });
