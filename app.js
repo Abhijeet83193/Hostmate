14277985e3bb62d603163856c7468f26d347e1bc
Abhijeet Dhokne <abhijeetdhokne1474@gmail.com>
2025-06-14 11:05:46 +0530

    add new route for ctreate the lists using req.body and make names obeject in ejs (listing[title]) & create the instance of the object assign into the newListing variable then created list redirect on the /listings Route again.

--- app.js
+++ app.js
@@ -39,6 +39,12 @@ app.get("/listings/:id", async (req, res) => {
     res.render("Listings/show.ejs", { listing });
 });
 
+//Create Route
+app.post("/listings", async (req, res) => {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+});
 
 
 // app.get("/testListing", async (req, res) => {
